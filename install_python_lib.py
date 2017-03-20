505a7e8c365662e34659721cb59765bb82140e2e
雷威 <leiwei@dingdatech.com>
2017-03-20 15:48:54 +0800

    commit

--- install_python_lib.py
+++ install_python_lib.py
@@ -8,12 +8,18 @@ import shutil
 def run():
     if not os.path.exists('/opt/python-lib'):
         os.mkdir('/opt/python-lib')
-    copyfile('pisces-python-lib.pth.sample', '/usr/lib/python2.7/site-packages/pisces-python-lib.pth')
-    copyfile('pisces-python-lib.pth.sample', '/usr/lib64/python2.7/site-packages/pisces-python-lib.pth')
+    copyfile(os.path.join(getcwd(), 'pisces-python-lib.pth.sample'),
+             '/usr/lib/python2.7/site-packages/pisces-python-lib.pth')
+    copyfile(os.path.join(getcwd(), 'pisces-python-lib.pth.sample'),
+             '/usr/lib64/python2.7/site-packages/pisces-python-lib.pth')
 
 
 def copyfile(src, dst):
     shutil.copyfile(src, dst)
+
+
+def getcwd():
+    return os.path.split(os.path.realpath(__file__))[0]
 
 
 if __name__ == '__main__':
