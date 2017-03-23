834a21e6a37ec0eaef0628150c6055484eeae73c
雷威 <leiwei@dingdatech.com>
2017-03-23 14:34:20 +0800

    c

--- install_springboot_service.py
+++ install_springboot_service.py
@@ -3,10 +3,12 @@
 
 import os
 import shutil
+import pisces.utils.sh as sh
 
 
 def add_springboot_systemd_service():
     copyfile(os.path.join(getcwd(), 'app.service'), '/etc/systemd/system/app.service')
+    sh.exe('chmod 744 /etc/systemd/system/app.service')
 
 
 def getcwd():
