f18c04026d734f0228964b0eb3899269d6cb3593
雷威 <leiwei@dingdatech.com>
2017-03-24 15:50:32 +0800

    c

--- run_disconf.py
+++ run_disconf.py
@@ -13,8 +13,8 @@ def run():
     # cmd = 'docker build -t {0}/disconf-build /opt/docker/docker-disconf/disconf-build'.format(config.docker_registry_url)
     # os.system(cmd)
 
-    cmd = 'docker run -v /opt/docker/docker-disconf/disconf-build/working:/home/work/dsp/disconf-rd/working -v /opt/docker/docker-disconf/disconf-build/config:/home/work/dsp/disconf-rd/online-resources --name disconf-build {0}/disconf-build'.format(config.docker_registry_url)
-    os.system(cmd)
+    # cmd = 'docker run -v /opt/docker/docker-disconf/disconf-build/working:/home/work/dsp/disconf-rd/working -v /opt/docker/docker-disconf/disconf-build/config:/home/work/dsp/disconf-rd/online-resources --name disconf-build {0}/disconf-build'.format(config.docker_registry_url)
+    # os.system(cmd)
 
     cmd = 'docker-compose -f /opt/docker/docker-disconf/disconf-compose/docker-compose.yml up'
     os.system(cmd)
