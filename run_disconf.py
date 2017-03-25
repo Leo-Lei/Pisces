cf3adbd6a0ef0f7cb6df72fa8a41aefd0b928436
雷威 <leiwei@dingdatech.com>
2017-03-25 16:07:59 +0800

    c

--- run_disconf.py
+++ run_disconf.py
@@ -1,23 +1,17 @@
 #! /usr/bin/python
 
 import os
-# import pisces.config as config
 import ConfigParser
 import pisces.utils.io as io
 
 
 def run():
-    # download_disconf()
     delete_all_containers()
     init_config()
-    cmd = 'docker build -t disconf-build /opt/docker/docker-disconf/disconf-build'
-    os.system(cmd)
 
-    cmd = 'docker run -v /opt/docker/docker-disconf/disconf-build/working:/home/work/dsp/disconf-rd/working -v /opt/docker/docker-disconf/disconf-build/config:/home/work/dsp/disconf-rd/online-resources --name disconf-build disconf-build'
-    os.system(cmd)
-
-    cmd = 'docker-compose -f /opt/docker/docker-disconf/disconf-compose/docker-compose.yml up'
-    os.system(cmd)
+    os.system('docker build -t disconf-build /opt/docker/docker-disconf/disconf-build')
+    os.system('docker run -v /opt/docker/docker-disconf/disconf-build/working:/home/work/dsp/disconf-rd/working -v /opt/docker/docker-disconf/disconf-build/config:/home/work/dsp/disconf-rd/online-resources --name disconf-build disconf-build')
+    os.system('docker-compose -f /opt/docker/docker-disconf/disconf-compose/docker-compose.yml up')
 
 
 def delete_all_containers():
@@ -50,21 +44,6 @@ def init_config():
     os.system('cp /opt/docker/docker-disconf/disconf-build/config/jdbc-mysql.properties.sample /opt/docker/docker-disconf/disconf-build/config/jdbc-mysql.properties')
     os.system('cp /opt/docker/docker-disconf/disconf-build/config/redis-config.properties.sample /opt/docker/docker-disconf/disconf-build/config/redis-config.properties')
     io.replace_str_in_file('/opt/docker/docker-disconf/disconf-build/config/zoo.properties.sample',{'${zookeeper-host}':disconf_machine_ip},'/opt/docker/docker-disconf/disconf-build/config/zoo.properties')
-
-
-# def download_disconf():
-#     cp = ConfigParser.SafeConfigParser()
-#     cp.read('/opt/app.conf')
-#     disconf_download_url = cp.get('docker-disconf', 'download_url')
-#     disconf_machine_ip = cp.get('docker-disconf','docker-disconf-machine-ip')
-#     os.system('curl {0} -o /opt/disconf-war.tar.gz'.format(disconf_download_url))
-#     os.system('tar -xzvf /opt/disconf-war.tar.gz -C /opt')
-#     os.system('chown -R tomcat:tomcat /opt/disconf-war')
-#
-#     os.system('cp /opt/docker/docker-disconf/disconf-build/config/application.properties.sample /opt/disconf-war/WEB-INF/classes/application.properties')
-#     os.system('cp /opt/docker/docker-disconf/disconf-build/config/jdbc-mysql.properties.sample /opt/disconf-war/WEB-INF/classes/jdbc-mysql.properties')
-#     os.system('cp /opt/docker/docker-disconf/disconf-build/config/redis-config.properties.sample /opt/disconf-war/WEB-INF/classes/redis-config.properties')
-#     io.replace_str_in_file('/opt/docker/docker-disconf/disconf-build/config/zoo.properties.sample','${zookeeper-host}',disconf_machine_ip,'/opt/disconf-war/WEB-INF/classes/zoo.properties')
 
 
 if __name__ == '__main__':
