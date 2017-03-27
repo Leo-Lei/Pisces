6f5eeba4c7231ae35298d144c439445343ded265
雷威 <leiwei@dingdatech.com>
2017-03-27 13:36:15 +0800

    c

--- install_mysql_client.py
+++ install_mysql_client.py
@@ -7,6 +7,7 @@ import pisces.utils.sh as sh
 def install_mysql_client():
     sh.exe('wget https://dev.mysql.com/get/mysql57-community-release-el7-9.noarch.rpm -O /opt/mysql.rpm')
     sh.exe('sudo rpm -Uvh /opt/mysql.rpm')
+    sh.exe('rm -f /opt/mysql.rpm')
     sh.exe('yum install -y mysql-community-client.x86_64')
 
 
