f7d5f84f407ed9596fc4be2c55ca4264b268593a
雷威 <leiwei@dingdatech.com>
2017-03-27 16:26:22 +0800

    c

--- run_mysql_init_sql.py
+++ run_mysql_init_sql.py
@@ -10,13 +10,21 @@ def run():
     os.system('curl -fSL 172.31.16.140/sql/create_database.sql -o /opt/create_database.sql')
     os.system('mysql -h 172.31.19.95 -P 3306 -uroot -proot < /opt/create_database.sql')
 
-    db_list = ['appconfig', 'comment', 'console', 'coupon', 'credit', 'device', 'finance', 'invite', 'mysql', 'push', 'qibeibike', 'usercenter']
+    db_list = ['appconfig', 'comment', 'console', 'coupon', 'credit', 'device', 'finance', 'invite', 'mysql', 'push',
+               'qibeibike', 'usercenter']
     for db in db_list:
         os.system('curl -fSL 172.31.16.140/sql/{0}.sql -o /opt/{0}.sql'.format(db))
         os.system('mysql -h 172.31.19.95 -P 3306 -uroot -proot -D{0} < /opt/{0}.sql'.format(db))
+
+    init_tables = {'credit': ['level_info', 'rule_config', 'score_rule']}
+
+    for (db, tables) in init_tables.items():
+        for table in tables:
+            os.system('curl -fSL 172.31.16.140/sql/{0}-{1}.sql -o /opt/{0}-{1}.sql'.format(db, table))
+            os.system('mysql -h 172.31.19.95 -P 3306 -uroot -proot -D{0} < /opt/{0}-{1}.sql'.format(db, table))
 
 
 if __name__ == '__main__':
     run()
 
-# curl -fSL http://apache.fayea.com/zookeeper/zookeeper-3.4.6/zookeeper-3.4.6.tar.gz -o zookeeper-3.4.6.tar.gz
+    # curl -fSL http://apache.fayea.com/zookeeper/zookeeper-3.4.6/zookeeper-3.4.6.tar.gz -o zookeeper-3.4.6.tar.gz
