08290dcf02bb827214bbb2aace3c06dd6a24309e
雷威 <leiwei@dingdatech.com>
2017-03-27 13:35:08 +0800

    c

---
+++ install_mysql_client.py
@@ -0,0 +1,18 @@
+#! /usr/bin/python
+# -*- coding: utf-8 -*-
+
+import pisces.utils.sh as sh
+
+
+def install_mysql_client():
+    sh.exe('wget https://dev.mysql.com/get/mysql57-community-release-el7-9.noarch.rpm -O /opt/mysql.rpm')
+    sh.exe('sudo rpm -Uvh /opt/mysql.rpm')
+    sh.exe('yum install -y mysql-community-client.x86_64')
+
+
+if __name__ == '__main__':
+    install_mysql_client()
+
+
+# Use below command to connect to mysql server
+# mysql -h 172.31.19.95 -P 3306 -uroot -proot
