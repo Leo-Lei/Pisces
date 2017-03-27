ce31680254c1c0bc37cac88ef5f2765f2445f8c4
雷威 <leiwei@dingdatech.com>
2017-03-27 15:54:45 +0800

    c

--- run_mysql_init_sql.py
+++ run_mysql_init_sql.py
@@ -13,7 +13,7 @@ def run():
     db_list = ['appconfig', 'comment', 'console', 'coupon', 'credit', 'device', 'finance', 'invite', 'mysql', 'push', 'qibeibike', 'usercenter']
     for db in db_list:
         os.system('curl -fSL 172.31.16.140/sql/{0}.sql -o /opt/{0}.sql'.format(db))
-        os.system('mysql -h 172.31.19.95 -P 3306 -uroot -proot < /opt/{0}.sql'.format(db))
+        os.system('mysql -h 172.31.19.95 -P 3306 -uroot -proot -D{0} < /opt/{0}.sql'.format(db))
 
 
 if __name__ == '__main__':
