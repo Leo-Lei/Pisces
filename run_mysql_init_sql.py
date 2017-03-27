671a8faeddaecd2f0004a938ae20fb4326488769
雷威 <leiwei@dingdatech.com>
2017-03-27 15:45:34 +0800

    c

---
+++ run_mysql_init_sql.py
@@ -0,0 +1,22 @@
+#! /usr/bin/python
+
+import os
+import sys
+import ConfigParser
+import pisces.utils.io as io
+
+
+def run():
+    os.system('curl -fSL 172.31.16.140/sql/create_database.sql -o /opt/create_database.sql')
+    os.system('mysql -h 172.31.19.95 -P 3306 -uroot -proot < /opt/create_database.sql')
+
+    db_list = ['appconfig', 'comment', 'console', 'coupon', 'credit', 'device', 'finance', 'invite', 'mysql', 'push', 'qibeibike', 'usercenter']
+    for db in db_list:
+        os.system('curl -fSL 172.31.16.140/sql/{0}.sql -o /opt/{0}.sql'.format(db))
+        os.system('mysql -h 172.31.19.95 -P 3306 -uroot -proot < /opt/{0}.sql'.format(db))
+
+
+if __name__ == '__main__':
+    run()
+
+# curl -fSL http://apache.fayea.com/zookeeper/zookeeper-3.4.6/zookeeper-3.4.6.tar.gz -o zookeeper-3.4.6.tar.gz
