962036579a610322354badc8e87c768a8b2ec16e
雷威 <leiwei@dingdatech.com>
2017-03-29 14:51:27 +0800

    c

--- install_tomcat.py
+++ install_tomcat.py
@@ -9,7 +9,7 @@ def install():
     os.system('curl -fSL 172.31.16.140/tomcat.zip -o /opt/tomcat.zip')
     os.system('unzip /opt/tomcat.zip -d /opt')
     os.system('mkdir /opt/tomcat')
-    os.system('cp -r /opt/apache-tomcat-8.5.12/* tomcat')
+    os.system('cp -r /opt/apache-tomcat-8.5.12/* /opt/tomcat')
     os.system('rm -rf /opt/apache-tomcat-8.5.12')
 
 
